d3f381d0b4a14a8ed72292e482dffdb61cce53e1
DimitryKamalov <74150180+DimitryKamalov@users.noreply.github.com>
2020-11-08 21:36:11 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onLoad; {
+    var user = prompt("Please enter your name");
+    alert('Hello, ' + user + ' welcome to my first blog! If you like it leave a like :v');
+}
+
+alert('Thanks to Volts for helping out at the hardest times');
+
+function prof() {
+    alert('Check my profile: https://www.sololearn.com/Profile/2696309');
+}
+
+function cd() {
+    alert('Check my others codes: https://www.sololearn.com/Codes?ordering=MyCodes&language=');
+}
